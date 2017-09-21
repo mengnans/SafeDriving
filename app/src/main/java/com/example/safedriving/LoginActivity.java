4270e7f36fbc95e55c693804f93007ceb659df3a
dang1337 <degray1990@gmail.com>
2017-09-21 17:21:20 +1000

    add activity for login screen

---
+++ app/src/main/java/com/example/safedriving/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.safedriving;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.zumoappname.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
