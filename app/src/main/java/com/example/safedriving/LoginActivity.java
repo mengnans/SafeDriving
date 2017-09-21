71abf29852f394588b91576b9500aea7c28a9a08
dang1337 <degray1990@gmail.com>
2017-09-21 18:39:52 +1000

    login text fields, buttons, get string values

--- app/src/main/java/com/example/safedriving/LoginActivity.java
+++ app/src/main/java/com/example/safedriving/LoginActivity.java
@@ -2,13 +2,35 @@ package com.example.safedriving;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameEditText = (EditText) findViewById(R.id.usernameText);
+        passwordEditText = (EditText) findViewById(R.id.passwordText);
+
     }
+
+    public void onClickLogin(View v) {
+        if (usernameEditText.getText().toString().matches("") || passwordEditText.getText().toString().matches("")) {
+            Log.d("","do nothing, chill");
+        }
+        else {
+            Log.d("",usernameEditText.getText().toString());
+            Log.d("",passwordEditText.getText().toString());
+        }
+
+    }
+
 }
