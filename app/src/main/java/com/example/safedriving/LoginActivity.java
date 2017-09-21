0a440cf9097cfc3de77731d8a4e22099fedd6924
dang1337 <degray1990@gmail.com>
2017-09-21 18:57:46 +1000

    add button that goes direct to MainActivity from login screen

--- app/src/main/java/com/example/safedriving/LoginActivity.java
+++ app/src/main/java/com/example/safedriving/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.safedriving;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 
@@ -11,6 +14,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText usernameEditText;
     private EditText passwordEditText;
+    private Button gotoMainActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,7 @@ public class LoginActivity extends AppCompatActivity {
 
         usernameEditText = (EditText) findViewById(R.id.usernameText);
         passwordEditText = (EditText) findViewById(R.id.passwordText);
+        gotoMainActivityButton = (Button) findViewById(R.id.gotoMainActivityButton);
 
     }
 
@@ -30,7 +35,13 @@ public class LoginActivity extends AppCompatActivity {
             Log.d("",usernameEditText.getText().toString());
             Log.d("",passwordEditText.getText().toString());
         }
+    }
 
+    public void onClickGotoMainActivity (View v) {
+        Context context = LoginActivity.this;
+        Class destinationActivity = SafeDrivingHomeActivity.class;
+        Intent startSafeDrivingHomeActivityIntent = new Intent(context, destinationActivity);
+        startActivity(startSafeDrivingHomeActivityIntent);
     }
 
 }
